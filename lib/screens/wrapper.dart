ed8158ecae42a00e2ed80c7ee5f39e57e33ddcd0
Omar Syed <omarsyed0504@gmail.com>
2024-01-20 23:34:12 -0500

    Added a method and logic for authentication from the log in page. Properly connected the login and password controls from firebase.

--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -1,11 +1,29 @@
 import 'package:flutter/material.dart';
-import 'package:team_shaikh_app/screens/authenticate/authenticate.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:team_shaikh_app/screens/authenticate/create_account.dart';
+import 'package:team_shaikh_app/screens/dashboard/dashboard.dart';
+import 'package:team_shaikh_app/screens/authenticate/login/login.dart';
 
-// Return either home or authenticate widget
+// Return either home or authenticate
 class Wrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // TODO: Return either dashboard or authenticate widget based on auth status.
-    return const Authenticate();
+    return Scaffold(
+      body: StreamBuilder<User?>(
+        stream: FirebaseAuth.instance.authStateChanges(), 
+        builder: (context, snapshot) {
+          // user is logged in
+          if (snapshot.hasData) {
+            return DashboardPage();
+          }
+
+          // user is not logged in
+          else {
+            return const LoginPage();
+          }
+
+        }
+      ),
+    );
   }
 }
