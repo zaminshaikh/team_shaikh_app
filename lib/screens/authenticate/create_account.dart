8558eb11606ac69b35e27b61343193221ce9db7f
Omar Syed <omarsyed0504@gmail.com>
2024-01-22 18:02:18 -0500

    Added dependencies

--- lib/screens/authenticate/create_account.dart
+++ lib/screens/authenticate/create_account.dart
@@ -1,4 +1,6 @@
 // Import Flutter Library
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:team_shaikh_app/screens/authenticate/login/login.dart';
 import 'package:firebase_auth/firebase_auth.dart';
