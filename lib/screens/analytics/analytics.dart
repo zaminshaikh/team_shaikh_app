09499fd14afcc9b320235bb24c096ae5881abf4f
Omar Syed <omarsyed0504@gmail.com>
2024-02-11 02:25:04 -0500

    Bottom navigation bar on the analytics page

--- lib/screens/analytics/analytics.dart
+++ lib/screens/analytics/analytics.dart
@@ -11,13 +11,13 @@ class AnalyticsPage extends StatefulWidget {
 
 class _AnalyticsPageState extends State<AnalyticsPage> {
 
-    int selectedIndex = 0;
-    List<IconData> data = [
-      Icons.home_outlined,
-      Icons.search,
-      Icons.add_box_outlined,
-      Icons.person_outline_sharp
-    ];
+  int selectedIndex = 0;
+  List<String> data = [
+    'assets/icons/dashboard_hollowed.png',
+    'assets/icons/analytics_hollowed.png',
+    'assets/icons/activity_hollowed.png',
+    'assets/icons/profile_hollowed.png',
+  ];
 
 
   @override
@@ -53,84 +53,87 @@ class _AnalyticsPageState extends State<AnalyticsPage> {
 
       ),
 
-      bottomNavigationBar: Container(
-        margin: const EdgeInsets.only(bottom: 50, right: 20, left: 20),
-        height: 80,
-        padding: const EdgeInsets.only(right: 30, left: 30),
-        decoration: BoxDecoration(
-          color: const Color.fromARGB(255, 37, 58, 86),
-          borderRadius: BorderRadius.circular(20),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.black.withOpacity(0.2),
-              spreadRadius: 2,
-              blurRadius: 5,
-              offset: const Offset(0, 3),
-            ),
-          ],
+      bottomNavigationBar: 
+  Container(
+    margin: const EdgeInsets.only(bottom: 50, right: 20, left: 20),
+    height: 80,
+    padding: const EdgeInsets.only(right: 30, left: 30),
+    decoration: BoxDecoration(
+      color: const Color.fromARGB(255, 30, 41, 59),
+      borderRadius: BorderRadius.circular(20),
+      boxShadow: [
+        BoxShadow(
+          color: Colors.black.withOpacity(0.2),
+          spreadRadius: 8,
+          blurRadius: 8,
+          offset: const Offset(0, 3),
         ),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: List.generate(
-            data.length,
-            (i) => GestureDetector(
-              onTap: () {
-                setState(() {
-                  selectedIndex = i;
-                });
+      ],
+    ),
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: List.generate(
+        data.length,
+        (i) => GestureDetector(
+          onTap: () {
+            setState(() {
+              selectedIndex = i;
+            });
 
-                if (data[i] == Icons.search) {
-                  Navigator.push(
-                    context,
-                    PageRouteBuilder(
-                      pageBuilder: (context, animation, secondaryAnimation) => const AnalyticsPage(),
-                      transitionsBuilder: (context, animation, secondaryAnimation, child) => child,
-                    ),
-                  );
-                }
+            if (data[i] == 'assets/icons/analytics_hollowed.png') {
+              Navigator.push(
+                context,
+                PageRouteBuilder(
+                  pageBuilder: (context, animation, secondaryAnimation) => const AnalyticsPage(),
+                  transitionsBuilder: (context, animation, secondaryAnimation, child) => child,
+                ),
+              );
+            }
 
-                if (data[i] == Icons.home_outlined) {
-                  Navigator.push(
-                    context,
-                    PageRouteBuilder(
-                      pageBuilder: (context, animation, secondaryAnimation) => DashboardPage(),
-                      transitionsBuilder: (context, animation, secondaryAnimation, child) => child,
-                    ),
-                  );
-                }
+            if (data[i] == 'assets/icons/dashboard_hollowed.png') {
+              Navigator.push(
+                context,
+                PageRouteBuilder(
+                  pageBuilder: (context, animation, secondaryAnimation) => DashboardPage(),
+                  transitionsBuilder: (context, animation, secondaryAnimation, child) => child,
+                ),
+              );
+            }
 
-                if (data[i] == Icons.add_box_outlined) {
-                  Navigator.push(
-                    context,
-                    PageRouteBuilder(
-                      pageBuilder: (context, animation, secondaryAnimation) => const ActivityPage(),
-                      transitionsBuilder: (context, animation, secondaryAnimation, child) => child,
-                    ),
-                  );
-                }
+            if (data[i] == 'assets/icons/activity_hollowed.png') {
+              Navigator.push(
+                context,
+                PageRouteBuilder(
+                  pageBuilder: (context, animation, secondaryAnimation) => const ActivityPage(),
+                  transitionsBuilder: (context, animation, secondaryAnimation, child) => child,
+                ),
+              );
+            }
 
-                if (data[i] == Icons.person_outline_sharp) {
-                  Navigator.push(
-                    context,
-                    PageRouteBuilder(
-                      pageBuilder: (context, animation, secondaryAnimation) => const ProfilePage(),
-                      transitionsBuilder: (context, animation, secondaryAnimation, child) => child,
-                    ),
-                  );
-                }
+            if (data[i] == 'assets/icons/profile_hollowed.png') {
+              Navigator.push(
+                context,
+                PageRouteBuilder(
+                  pageBuilder: (context, animation, secondaryAnimation) => const ProfilePage(),
+                  transitionsBuilder: (context, animation, secondaryAnimation, child) => child,
+                ),
+              );
+            }
 
-              },
-              child: Icon(
-                data[i],
-                size: 35,
-                color: data[i] == Icons.search
-                ? Colors.white 
-                : Colors.blueGrey,
-              ),
-            ),
-          ),
+          },
+
+          child: Image.asset(
+            i == selectedIndex && data[i] == 'assets/icons/analytics_hollowed.png'
+              ? 'assets/icons/analytics_filled.png'
+              : data[i],
+            height: 50,
+          ),       
         ),
       ),
+    ),    
+  )
+  
+
           
     );
 }
