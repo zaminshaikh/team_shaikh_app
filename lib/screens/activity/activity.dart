ccf6ee68856ebaa287557261dc110f3206683782
Omar Syed <omarsyed0504@gmail.com>
2024-02-26 20:00:16 -0500

    Made a notification page

--- lib/screens/activity/activity.dart
+++ lib/screens/activity/activity.dart
@@ -10,7 +10,6 @@ import 'package:team_shaikh_app/screens/analytics/analytics.dart';
 import 'package:team_shaikh_app/database.dart';
 import 'package:team_shaikh_app/screens/profile/profile.dart';
 
-
 class ActivityPage extends StatefulWidget {
   const ActivityPage({Key? key}) : super(key: key);
   @override
@@ -27,7 +26,6 @@ class _ActivityPageState extends State<ActivityPage> {
   ];
 
   late DatabaseService _databaseService;
-  
 
   Future<void> _initData() async {
     // If the user is signed in (which should always be the case on this screen)
@@ -99,25 +97,46 @@ class _ActivityPageState extends State<ActivityPage> {
   );
 
   Scaffold _buildActivityScreen(AsyncSnapshot<List<Map<String, dynamic>>> activitiesSnapshot) => Scaffold(
-      body: CustomScrollView(
-        slivers: <Widget>[
-          _buildAppBar(),
-          SliverList(
-            delegate: SliverChildBuilderDelegate(
-              (context, index) {
-                final activities = activitiesSnapshot.data!;
-                activities.sort((a, b) => b['time'].compareTo(a['time'])); // Sort the list by time in reverse order
-                final activity = activities[index];
-                return _buildActivityWithDayHeader(activity, index, activities);
-              },
-              childCount: activitiesSnapshot.data!.length,
+    body: Stack(
+      children: [
+        CustomScrollView(
+          slivers: <Widget>[
+            _buildAppBar(),
+            SliverPadding(
+              padding: const EdgeInsets.only(top: 20.0),
+              sliver: SliverList(
+                delegate: SliverChildBuilderDelegate(
+                  (context, index) {
+                    if (index == 0) {
+                      return _buildSearchBar();
+                    } else if (index == 1) {
+                      return _buildHorizontalButtonList([]); // Add your button list here
+                    } else {
+                      final activities = activitiesSnapshot.data!;
+                      activities.sort((a, b) => b['time'].compareTo(a['time'])); // Sort the list by time in reverse order
+                      final activity = activities[index - 2]; // Subtract 2 because the first index is used by the search bar and the second by the button list
+                      return _buildActivityWithDayHeader(activity, index - 2, activities);
+                    }
+                  },
+                  childCount: activitiesSnapshot.data!.length + 2, // Add 2 to include the search bar and the button list
+                ),
+              ),
             ),
-          ),
-        ],
-      ),
-      bottomNavigationBar: _buildBottomNavBar(),
-    );
-
+            SliverToBoxAdapter(
+              child: const SizedBox(height: 150.0), // Add some space at the bottom
+            ),
+          ],
+        ),
+        Positioned(
+          left: 0,
+          right: 0,
+          bottom: 0,
+          child: _buildBottomNavBar(),
+        ),
+      ],
+    ),
+  );  
+  
   SliverAppBar _buildAppBar() => SliverAppBar(
     backgroundColor: const Color.fromARGB(255, 30, 41, 59),
     automaticallyImplyLeading: false,
@@ -154,172 +173,226 @@ class _ActivityPageState extends State<ActivityPage> {
       actions: [
         Padding(
           padding: const EdgeInsets.only(right: 10.0),
-          child: Image.asset(
-            'assets/icons/notification_bell.png',
-            color: Colors.white,
-            height: 32,
-            width: 32,
+          child: GestureDetector(
+            onTap: () {
+              Navigator.pushNamed(context, '/notification');
+            },
+            child: Image.asset(
+              'assets/icons/notification_bell.png',
+              color: Colors.white,
+              height: 32,
+              width: 32,
+            ),
           ),
         ),
       ],
     );
   
-  /// If the activity is on a new day, we create a header stating the day.
+  // If the activity is on a new day, we create a header stating the day.
   Widget _buildActivityWithDayHeader(Map<String, dynamic> activity, int index, List<Map<String, dynamic>> activities) {
     final activityDate = (activity['time'] as Timestamp).toDate();
     final previousActivityDate = index > 0 ? (activities[index - 1]['time'] as Timestamp).toDate() : null;
+    final nextActivityDate = index < activities.length - 1 ? (activities[index + 1]['time'] as Timestamp).toDate() : null;
+    final firstActivityDate = (activities[0]['time'] as Timestamp).toDate();
+
+    bool isLastActivityForTheDay = nextActivityDate == null || !_isSameDay(activityDate, nextActivityDate);
+    bool isLatestDate = _isSameDay(activityDate, firstActivityDate);
 
     if (previousActivityDate == null || !_isSameDay(activityDate, previousActivityDate)) {
       return Column(
         children: <Widget>[
-          Text(
-            DateFormat('EEEE, MMMM d, yyyy').format(activityDate),
-            textAlign: TextAlign.left,
-            style: const TextStyle(color: AppColors.defaultWhite),
-            ), // Day header
-          _buildActivity(activity), // Activity
-        ],
-      );
-    } else {
-      return _buildActivity(activity);
-    }
-  }
-
-  Widget _buildActivity(Map<String, dynamic> activity) { 
-        // Assuming activity['time'] is a Timestamp object
-    Timestamp timestamp = activity['time'];
-
-    // Convert the Timestamp to a DateTime
-    DateTime dateTime = timestamp.toDate();
-
-    // Create a new DateFormat for the desired time format
-    DateFormat format = DateFormat('hh:mm a');
-
-    // Use the format to format the dateTime
-    String time = format.format(dateTime);
-    
-    return Row(
-      children: [
-        Icon(
-          Icons.circle,
-          color: Color.fromARGB(255, 19, 66, 105),
-          size: 50,
-        ),
-        const SizedBox(width: 10),
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              activity['fund'],
-              style: const TextStyle(
-                fontSize: 18,
-                color: Colors.white,
-                fontWeight: FontWeight.w600,
-                fontFamily: 'Titillium Web',
-              ),
-            ),
-            const SizedBox(height: 5),
-            Text(
-              getActivityType(activity),
-              style: const TextStyle(
-                fontSize: 15,
-                color: Colors.blue,
-                fontWeight: FontWeight.bold,
-                fontFamily: 'Titillium Web',
-              ),
-            ),
-          ],
-        ),
-        const Spacer(),
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            Align(
-              alignment: Alignment.centerRight,
+          Padding(
+            padding: EdgeInsets.fromLTRB(20.0, isLatestDate ? 20.0 : 40.0, 20.0, 25.0), // Add padding to the top only if it's not the latest date
+            child: Align(
+              alignment: Alignment.centerLeft,
               child: Text(
-                currencyFormat(activity['amount'].toDouble()),
-                style: const TextStyle(
-                  fontSize: 18,
-                  color: Colors.blue,
+                DateFormat('MMMM d, yyyy').format(activityDate),
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.white,
                   fontWeight: FontWeight.bold,
                   fontFamily: 'Titillium Web',
                 ),
               ),
             ),
-            SizedBox(height: 5),
-            Row(
+          ), // Day header
+          _buildActivity(activity, !isLastActivityForTheDay), // Activity
+        ],
+      );
+    } else {
+      return _buildActivity(activity, !isLastActivityForTheDay);
+    }
+  }
+  
+  Widget _buildActivity(Map<String, dynamic> activity, bool showDivider, ) { 
+
+        // Assuming activity['time'] is a Timestamp object
+        Timestamp timestamp = activity['time'];
+
+        // Convert the Timestamp to a DateTime
+        DateTime dateTime = timestamp.toDate();
+
+        // Create a new DateFormat for the desired time format
+        DateFormat format = DateFormat('h:mm a'); 
+
+        // Use the format to format the dateTime
+        String time = format.format(dateTime);
+
+        Color getColorBasedOnActivityType(String activityType) {
+          switch (activityType) {
+            case 'deposit':
+              return Colors.green;
+            case 'withdrawal':
+              return Colors.red;
+            case 'income':
+              return Colors.blue;
+            default:
+              return Colors.blue;
+          }
+        }
+        
+      return Column(
+        children: [
+          
+          Padding(
+            padding: EdgeInsets.fromLTRB(25.0, 5.0, 25.0, 5.0),
+            child: Row(
               children: [
-                Text(
-                  time,
-                  style: TextStyle(
-                    fontSize: 15,
-                    color: Colors.white,
-                    fontFamily: 'Titillium Web',
-                  ),
+                Icon(
+                  Icons.circle,
+                  color: getColorBasedOnActivityType(activity['type']),
+                  size: 50,
                 ),
-                SizedBox(width: 7), // Add width
-                Container(
-                  height: 15, // You can adjust the height as needed
-                  child: const VerticalDivider(
-                    color: Colors.white,
-                    width: 1,
-                    thickness: 1,
-                  ),
+                const SizedBox(width: 10),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      activity['fund'],
+                      style: const TextStyle(
+                        fontSize: 18,
+                        color: Colors.white,
+                        fontWeight: FontWeight.w600,
+                        fontFamily: 'Titillium Web',
+                      ),
+                    ),
+                    const SizedBox(height: 5),
+                    Text(
+                      getActivityType(activity),
+                      style: TextStyle(
+                        fontSize: 15,
+                        color: getColorBasedOnActivityType(activity['type']),
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'Titillium Web',
+                      ),
+                    ),
+                  ],
                 ),
-                SizedBox(width: 7), // Add width
-                Text(
-                  activity['recipient'],
-                  style: const TextStyle(
-                    fontSize: 15,
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                    fontFamily: 'Titillium Web',
-                  ),
+                const Spacer(),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Align(
+                      alignment: Alignment.centerRight,
+                      child: Text(
+                        currencyFormat(activity['amount'].toDouble()),
+                        style: TextStyle(
+                          fontSize: 18,
+                          color: getColorBasedOnActivityType(activity['type']),
+                          fontWeight: FontWeight.bold,
+                          fontFamily: 'Titillium Web',
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 5),
+                    Row(
+                      children: [
+                        Text(
+                          time,
+                          style: TextStyle(
+                            fontSize: 13,
+                            color: Colors.white,
+                            fontFamily: 'Titillium Web',
+                          ),
+                        ),
+                        SizedBox(width: 7), // Add width
+                        Container(
+                          height: 15, // You can adjust the height as needed
+                          child: const VerticalDivider(
+                            color: Colors.white,
+                            width: 1,
+                            thickness: 1,
+                          ),
+                        ),
+                        SizedBox(width: 7), // Add width
+                        Text(
+                          activity['recipient'],
+                          style: const TextStyle(
+                            fontSize: 13,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontFamily: 'Titillium Web',
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
                 ),
               ],
             ),
-          ],
-        ),
-      ],
-    );
-  }
-
-  Container _buildHorizontalButtonList(Map<String, String> userName) => Container(
-      height: 40.0, // Make the buttons a little shorter
-      child: ListView(
-        scrollDirection: Axis.horizontal,
-        children: <Widget>[
-          const SizedBox(width: 20.0), // Add some space before the first button
-          ElevatedButton(
-            child: const Text(
-              'All',
-              style: TextStyle(
-                fontFamily: 'Titillium Web',
-                color: Colors.white, // Make the text white
-              ),
-            ),
-            style: ElevatedButton.styleFrom(
-              primary: Colors.transparent,
-              side: const BorderSide(color: Colors.grey),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(20.0),
-              ),
-            ),
-            onPressed: () {
-              // Implement your button functionality here
-            },
           ),
-          const SizedBox(width: 10.0), // Add some space between the buttons
-          ElevatedButton(
+          
+          if (showDivider)
+            Padding(
+              padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+              child: const Divider(
+                color: Color.fromARGB(255, 132, 132, 132),
+                thickness: 0.2,
+              ),
+            )
+        ],
+      );
+    }
+
+  Container _buildHorizontalButtonList(List<Map<String, String>> users) => Container(
+    height: 35.0, 
+    child: ListView(
+      scrollDirection: Axis.horizontal,
+      children: <Widget>[
+        const SizedBox(width: 20.0), // Add some space before the first button
+        ElevatedButton(
+          child: const Text(
+            'All',
+            style: TextStyle(
+              fontFamily: 'Titillium Web',
+              fontSize: 16,
+              color: Colors.white, // Make the text white
+            ),
+          ),
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.transparent,
+            side: const BorderSide(color: Colors.grey),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20.0),
+            ),
+          ),
+          onPressed: () {
+            // Implement your button functionality here
+          },
+        ),
+        const SizedBox(width: 10.0), // Add some space between the buttons
+        ...users.map((user) => Padding(
+          padding: const EdgeInsets.only(right: 10.0),
+          child: ElevatedButton(
             child: Text(
-              '${userName['first']} ${userName['last']}',
+              '${user['first']} ${user['last']}',
               style: const TextStyle(
                 fontFamily: 'Titillium Web',
                 color: Colors.white,
               ),
             ),
             style: ElevatedButton.styleFrom(
-              primary: Colors.transparent,
+              backgroundColor: Colors.transparent,
               side: const BorderSide(color: Colors.grey),
               shape: RoundedRectangleBorder(
                 borderRadius: BorderRadius.circular(20.0),
@@ -329,53 +402,14 @@ class _ActivityPageState extends State<ActivityPage> {
               // Implement your button functionality here
             },
           ),
-          const SizedBox(width: 10.0),
-          ElevatedButton(
-            child: const Text(
-              'Withdrawals',
-              style: TextStyle(
-                fontFamily: 'Titillium Web',
-                color: Colors.white,
-              ),
-            ),
-            style: ElevatedButton.styleFrom(
-              primary: Colors.transparent,
-              side: const BorderSide(color: Colors.grey),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(20.0),
-              ),
-            ),
-            onPressed: () {
-              // Implement your button functionality here
-            },
-          ),
-          const SizedBox(width: 10.0),
-          ElevatedButton(
-            child: const Text(
-              'Pending',
-              style: TextStyle(
-                fontFamily: 'Titillium Web',
-                color: Colors.white,
-              ),
-            ),
-            style: ElevatedButton.styleFrom(
-              primary: Colors.transparent,
-              side: const BorderSide(color: Colors.grey),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(20.0),
-              ),
-            ),
-            onPressed: () {
-              // Implement your button functionality here
-            },
-          ),
-        ],
-      ),
-    );
+        )).toList(),
+      ],
+    ),
+  );
 
   // This is the search bar area 
-  Padding _buildSearchBar() => Padding(
-      padding: const EdgeInsets.all(20.0),
+    Padding _buildSearchBar() => Padding(
+      padding: const EdgeInsets.fromLTRB(20.0,10,20,25),
       child: Container(
         child: Row(
           children: [
