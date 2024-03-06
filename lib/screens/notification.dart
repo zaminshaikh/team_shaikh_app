ca6af91208b73cd6ee2d2487c3962478c497d555
Zamin Shaikh <zaminusa@gmail.com>
2024-03-06 13:45:11 -0500

    Added notification.dart

---
+++ lib/screens/notification.dart
@@ -0,0 +1,153 @@
+import 'dart:developer';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:team_shaikh_app/screens/dashboard/dashboard.dart';
+import 'package:team_shaikh_app/database.dart';
+import 'package:team_shaikh_app/screens/profile/profile.dart';
+import 'package:intl/intl.dart';
+import 'package:team_shaikh_app/screens/activity/activity.dart';
+
+class NotificationPage extends StatefulWidget {
+  const NotificationPage({Key? key}) : super(key: key);
+  @override
+  _NotificationPageState createState() => _NotificationPageState();
+}
+
+class _NotificationPageState extends State<NotificationPage> {
+
+  // database service instance
+  late DatabaseService _databaseService;
+
+  Future<void> _initData() async {
+
+    User? user = FirebaseAuth.instance.currentUser;
+      if (user == null) {
+        log('User is not logged in');
+        Navigator.pushReplacementNamed(context, '/login');
+      }
+    // Fetch CID using async constructor
+    DatabaseService? service = await DatabaseService.fetchCID(user!.uid, 1);
+    // If there is no matching CID, redirect to login page
+    if (service == null) {
+      Navigator.pushReplacementNamed(context, '/login');
+    } else {
+      // Otherwise set the database service instance
+      _databaseService = service;
+      log('Database Service has been initialized with CID: ${_databaseService.cid}');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) => FutureBuilder(
+    future: _initData(), // Initialize the database service
+    builder: (context, snapshot) {
+      if (snapshot.connectionState == ConnectionState.waiting) {
+        return const Center(
+          child: CircularProgressIndicator(),
+        );
+      }
+      return StreamBuilder<UserWithAssets>(
+        stream: _databaseService.getUserWithAssets,
+        builder: (context, userSnapshot) {
+          // Wait for the user snapshot to have data
+          if (!userSnapshot.hasData || userSnapshot.data == null) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          // Once we have the user snapshot, we can build the activity page
+          return buildActivityPage(userSnapshot);
+        }
+      );
+    }
+  );  
+  
+  Scaffold buildActivityPage(AsyncSnapshot<UserWithAssets> userSnapshot) {
+    
+    UserWithAssets user = userSnapshot.data!;
+    String firstName = user.info['name']['first'] as String;
+    String lastName = user.info['name']['last'] as String;
+    String companyName = user.info['name']['company'] as String;
+    Map<String, String> userName = {'first': firstName, 'last': lastName, 'company': companyName};
+       
+      return Scaffold(
+        body: Stack(
+          children: [
+            CustomScrollView(
+              slivers: <Widget>[
+                _buildAppBar(context),
+                SliverPadding(
+                  padding: const EdgeInsets.all(0.0),
+                  sliver: SliverList(
+                    delegate: SliverChildListDelegate(
+                      [
+                        // Total assets section
+
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      );
+      
+  }
+
+
+  
+}
+
+// This is the app bar 
+  SliverAppBar _buildAppBar(context) {
+    return SliverAppBar(
+    backgroundColor: const Color.fromARGB(255, 30, 41, 59),
+    toolbarHeight: 80,
+    expandedHeight: 0,
+    snap: false,
+    floating: true,
+    pinned: true,
+    leading: IconButton(
+      icon: const Icon(Icons.arrow_back_ios_new_rounded, color: Colors.white),
+      onPressed: () {
+        Navigator.of(context).pop();
+      },
+    ),
+    flexibleSpace: SafeArea(
+      child: Stack(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 60.0, right: 20.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Notifications',
+                  style: TextStyle(
+                    fontSize: 27,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Titillium Web',
+                  ),
+                ),
+              ],
+            ),
+          ),
+          
+        ],
+      ),
+    ),
+      actions: [
+        Padding(
+          padding: const EdgeInsets.only(right: 10.0),
+          child: IconButton(
+            icon: const Icon(Icons.settings, color: Colors.white, size: 30.0),
+            onPressed: () {},
+          )
+        ),
+      ],
+    );
+  }
+
