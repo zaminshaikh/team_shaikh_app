4a4a29346e88e9f4e208f2f648ca7e4cfb8e2da3
Omar Syed <omarsyed0504@gmail.com>
2024-05-09 19:16:01 -0400

    Cleaned the look of the custom alert dialog

--- lib/utilities.dart
+++ lib/utilities.dart
@@ -3,6 +3,7 @@ import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:team_shaikh_app/resources.dart';
 import 'package:team_shaikh_app/screens/activity/activity.dart';
 import 'package:team_shaikh_app/screens/analytics/analytics.dart';
 import 'package:team_shaikh_app/screens/dashboard/dashboard.dart';
@@ -22,30 +23,50 @@ import 'package:team_shaikh_app/screens/profile/profile.dart';
 /// CustomAlertDialog.showAlertDialog(context, 'Alert', 'This is an alert message');
 /// ```
 class CustomAlertDialog {
-  static Future<void> showAlertDialog(BuildContext context, String title, String message) async => showDialog<void>(
+  static Future<void> showAlertDialog(BuildContext context, String title, String message, {Icon? icon}) async => showDialog<void>(
       context: context,
       barrierDismissible: false, // user must tap button!
       builder: (BuildContext context) => AlertDialog(
-          title: Text(title),
+        backgroundColor: AppColors.defaultBlueGray800,
           content: SingleChildScrollView(
             child: ListBody(
               children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 10),
+                  child: Row(
+                  children: <Widget>[
+                    Text(title, style: Theme.of(context).textTheme.titleLarge),
+                    const SizedBox(width: 10),
+                    icon ?? const Icon(Icons.square, color: Colors.transparent),
+                  ],
+                  ),
+                ),
                 Text(message),
               ],
             ),
           ),
           actions: <Widget>[
-            TextButton(
-              child: const Text('Okay'),
-              onPressed: () {
+            GestureDetector(
+              onTap: () {
                 Navigator.of(context).pop();
               },
-            ),
+              child: Container(
+                width: double.infinity, // This will make the container take up the full width of the AlertDialog
+                padding: EdgeInsets.symmetric(vertical: 10), // Add some vertical padding
+                decoration: BoxDecoration(
+                  color: const Color.fromARGB(255, 30, 75, 137), // Change this to the color you want
+                  borderRadius: BorderRadius.circular(20), // This will make the corners rounded
+                ),
+                child: Text(
+                  'Continue',
+                  textAlign: TextAlign.center, // This will center the text
+                ),
+              ),
+            )
           ],
         ),
     );
 }
-
 /// Formats the given amount as a currency string.
 String currencyFormat(double amount) => NumberFormat.currency(
     symbol: '\$',
