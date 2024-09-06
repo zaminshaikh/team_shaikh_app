9edca53b92e9a7e5b938aa139de9da6546d5f804
Zamin Shaikh <zaminusa@gmail.com>
2024-09-06 00:56:01 -0400

    Made circular progress indicator in main transparent

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,6 @@ import 'package:team_shaikh_app/screens/authenticate/faceid.dart';
 import 'package:team_shaikh_app/screens/authenticate/app_state.dart';
 import 'package:provider/provider.dart';
 
-
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await ScreenUtil.ensureScreenSize();
@@ -51,7 +50,6 @@ class MyApp extends StatefulWidget {
   MyAppState createState() => MyAppState();
 }
 
-
 class MyAppState extends State<MyApp> with WidgetsBindingObserver {
   final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
   AppState? appState;
@@ -73,53 +71,51 @@ class MyAppState extends State<MyApp> with WidgetsBindingObserver {
   void didChangeAppLifecycleState(AppLifecycleState state) {
     // Update the lifecycle state
     final appState = Provider.of<AppState>(context, listen: false);
- 
-  
+
     if ((state == AppLifecycleState.paused ||
             state == AppLifecycleState.inactive ||
             state == AppLifecycleState.hidden) &&
-            !appState.hasNavigatedToFaceIDPage &&
-            isAuthenticated() &&
-            (appState.initiallyAuthenticated)) { // Check if the user was initially authenticated
-          
-          appState.setHasNavigatedToFaceIDPage(true);
-          navigatorKey.currentState?.pushReplacement(
-            PageRouteBuilder(
-              pageBuilder: (context, animation, secondaryAnimation) => const FaceIdPage(),
-              transitionsBuilder: (context, animation, secondaryAnimation, child) => child,
-            ),
-          );
-        } else {
-          if (appState.hasNavigatedToFaceIDPage) {
-          }
-          if (!isAuthenticated()) {
-          }
-          if (!appState.initiallyAuthenticated) {
-          }
-    
-          if (appState.justAuthenticated) {
-            appState.setHasNavigatedToFaceIDPage(false);
-            appState.setJustAuthenticated(false);
-          }
-          if (appState.hasNavigatedToFaceIDPage) {
-          } else {
-          }
-        }
-      }  
+        !appState.hasNavigatedToFaceIDPage &&
+        isAuthenticated() &&
+        (appState.initiallyAuthenticated)) {
+      // Check if the user was initially authenticated
 
+      appState.setHasNavigatedToFaceIDPage(true);
+      navigatorKey.currentState?.pushReplacement(
+        PageRouteBuilder(
+          pageBuilder: (context, animation, secondaryAnimation) =>
+              const FaceIdPage(),
+          transitionsBuilder: (context, animation, secondaryAnimation, child) =>
+              child,
+        ),
+      );
+    } else {
+      if (appState.hasNavigatedToFaceIDPage) {}
+      if (!isAuthenticated()) {}
+      if (!appState.initiallyAuthenticated) {}
+
+      if (appState.justAuthenticated) {
+        appState.setHasNavigatedToFaceIDPage(false);
+        appState.setJustAuthenticated(false);
+      }
+      if (appState.hasNavigatedToFaceIDPage) {
+      } else {}
+    }
+  }
 
   bool isAuthenticated() {
     final user = FirebaseAuth.instance.currentUser;
     return user != null;
   }
-  
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       navigatorKey: navigatorKey,
       builder: (context, child) => MediaQuery(
         data: MediaQuery.of(context).copyWith(
-          boldText: false, textScaler: const TextScaler.linear(1),
+          boldText: false,
+          textScaler: const TextScaler.linear(1),
         ),
         child: child!,
       ),
@@ -139,18 +135,18 @@ class MyAppState extends State<MyApp> with WidgetsBindingObserver {
               color: Colors.white,
               fontFamily: 'Titillium Web',
               fontWeight: FontWeight.bold),
-          labelLarge: TextStyle(
-              color: Colors.white, fontFamily: 'Titillium Web'),
-          labelMedium: TextStyle(
-              color: Colors.white, fontFamily: 'Titillium Web'),
-          labelSmall: TextStyle(
-              color: Colors.white, fontFamily: 'Titillium Web'),
-          displayLarge: TextStyle(
-              color: Colors.white, fontFamily: 'Titillium Web'),
-          displayMedium: TextStyle(
-              color: Colors.white, fontFamily: 'Titillium Web'),
-          displaySmall: TextStyle(
-              color: Colors.white, fontFamily: 'Titillium Web'),
+          labelLarge:
+              TextStyle(color: Colors.white, fontFamily: 'Titillium Web'),
+          labelMedium:
+              TextStyle(color: Colors.white, fontFamily: 'Titillium Web'),
+          labelSmall:
+              TextStyle(color: Colors.white, fontFamily: 'Titillium Web'),
+          displayLarge:
+              TextStyle(color: Colors.white, fontFamily: 'Titillium Web'),
+          displayMedium:
+              TextStyle(color: Colors.white, fontFamily: 'Titillium Web'),
+          displaySmall:
+              TextStyle(color: Colors.white, fontFamily: 'Titillium Web'),
           headlineLarge: TextStyle(
               color: Colors.white,
               fontFamily: 'Titillium Web',
@@ -163,12 +159,12 @@ class MyAppState extends State<MyApp> with WidgetsBindingObserver {
               color: Colors.white,
               fontFamily: 'Titillium Web',
               fontWeight: FontWeight.bold),
-          bodyLarge: TextStyle(
-              color: Colors.white, fontFamily: 'Titillium Web'),
-          bodyMedium: TextStyle(
-              color: Colors.white, fontFamily: 'Titillium Web'),
-          bodySmall: TextStyle(
-              color: Colors.white, fontFamily: 'Titillium Web'),
+          bodyLarge:
+              TextStyle(color: Colors.white, fontFamily: 'Titillium Web'),
+          bodyMedium:
+              TextStyle(color: Colors.white, fontFamily: 'Titillium Web'),
+          bodySmall:
+              TextStyle(color: Colors.white, fontFamily: 'Titillium Web'),
         ),
       ),
       home: const AuthChecker(),
@@ -190,32 +186,44 @@ class MyAppState extends State<MyApp> with WidgetsBindingObserver {
 class AuthChecker extends StatelessWidget {
   const AuthChecker({Key? key}) : super(key: key);
 
-  Future<DatabaseService?> _fetchDatabaseService(BuildContext context, String uid) async {
+  Future<DatabaseService?> _fetchDatabaseService(
+      BuildContext context, String uid) async {
     return await DatabaseService.fetchCID(context, uid, 1);
   }
 
   @override
   Widget build(BuildContext context) {
     return StreamBuilder<User?>(
-      stream: FirebaseAuth.instance.userChanges(), // Stream that listens for changes in the user's authentication state
+      stream: FirebaseAuth.instance
+          .userChanges(), // Stream that listens for changes in the user's authentication state
       builder: (context, snapshot) {
         if (snapshot.connectionState == ConnectionState.waiting) {
-          return const CircularProgressIndicator(); // Show a loading indicator while waiting for the authentication state
+          return const CircularProgressIndicator(
+            valueColor: AlwaysStoppedAnimation<Color>(Colors.transparent),
+            strokeWidth: 6.0,
+          ); // Show a loading indicator while waiting for the authentication state
         } else if (snapshot.hasError) {
           log('main.dart: StreamBuilder error: ${snapshot.error}'); // Log any errors that occur during the stream
-          return Text('Error: ${snapshot.error}'); // Show an error message if there is an error in the stream
+          return Text(
+              'Error: ${snapshot.error}'); // Show an error message if there is an error in the stream
         } else if (snapshot.hasData) {
-          final user = snapshot.data!; // Get the authenticated user from the snapshot
+          final user =
+              snapshot.data!; // Get the authenticated user from the snapshot
           log('main.dart: User is logged in as ${user.email}'); // Log the user's email
           return FutureBuilder<DatabaseService?>(
             future: _fetchDatabaseService(context, user.uid),
             builder: (context, serviceSnapshot) {
               if (serviceSnapshot.connectionState == ConnectionState.waiting) {
-                return const CircularProgressIndicator(); // Show a loading indicator while waiting for the Firestore query
+                return const CircularProgressIndicator(
+                  valueColor: AlwaysStoppedAnimation<Color>(Colors.transparent),
+                  strokeWidth: 6.0,
+                ); // Show a loading indicator while waiting for the Firestore query
               } else if (serviceSnapshot.hasError) {
                 log('main.dart: Firestore query error: ${serviceSnapshot.error}'); // Log any errors that occur during the Firestore query
-                return Text('Error: ${serviceSnapshot.error}'); // Show an error message if there is an error in the Firestore query
-              } else if (serviceSnapshot.hasData && serviceSnapshot.data != null) {
+                return Text(
+                    'Error: ${serviceSnapshot.error}'); // Show an error message if there is an error in the Firestore query
+              } else if (serviceSnapshot.hasData &&
+                  serviceSnapshot.data != null) {
                 log('main.dart: UID found in Firestore.'); // Log that the UID was found in Firestore
                 return const InitialFaceIdPage(); // If the UID is found, show the FaceIdPage
               } else {
