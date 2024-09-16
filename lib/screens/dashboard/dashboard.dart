3cb3606bc2f29d8aa033c5f11c82af89197212b6
Zamin Shaikh <zaminusa@gmail.com>
2024-09-16 14:17:16 -0400

    Added the appBar and navBar for Dashboard, refactored to support the Client class

--- lib/screens/dashboard/dashboard.dart
+++ lib/screens/dashboard/dashboard.dart
@@ -1,13 +1,16 @@
 import 'dart:developer';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:team_shaikh_app/components/progress_indicator.dart';
-import 'package:team_shaikh_app/database.dart';
 import 'package:team_shaikh_app/database/models/client_model.dart';
+import 'package:team_shaikh_app/screens/activity/activity.dart';
+import 'package:team_shaikh_app/screens/analytics/analytics.dart';
 import 'package:team_shaikh_app/screens/authenticate/app_state.dart';
-import 'package:team_shaikh_app/database/newdb.dart';
+import 'package:team_shaikh_app/screens/notification.dart';
+import 'package:team_shaikh_app/screens/profile/profile.dart';
 
 class DashboardPage extends StatefulWidget {
   final bool fromFaceIdPage;
@@ -18,9 +21,7 @@ class DashboardPage extends StatefulWidget {
   _DashboardPageState createState() => _DashboardPageState();
 }
 
-class _DashboardPageState extends State<DashboardPage>
-    with SingleTickerProviderStateMixin {
-  // database service instance
+class _DashboardPageState extends State<DashboardPage> with SingleTickerProviderStateMixin {
   AuthState? authState;
   Client? client;
   late AnimationController _controller;
@@ -131,10 +132,6 @@ class _DashboardPageState extends State<DashboardPage>
             Text('Company: ${client!.companyName}'),
             Text('Total Assets: \$${client!.totalAssets}'),
             const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: _handleButtonPress, // Example action handler
-              child: const Text('Refresh Data'),
-            ),
             // Add more client-related data and actions here
           ],
         ),
@@ -142,12 +139,307 @@ class _DashboardPageState extends State<DashboardPage>
     );
   }
 
-  // Example action method to demonstrate state changes
-  void _handleButtonPress() {
-    setState(() {
-      // You can modify the state or trigger actions here that will re-render the widget
-      // For example, you might want to fetch some additional data or update a UI element
-      // The state will be updated and the UI will refresh when setState is called
-    });
+  // ignore: prefer_expression_function_bodies
+  Scaffold _dashboard() {
+
+    return Scaffold(
+      body: Stack(
+        children: [
+          CustomScrollView(
+            slivers: <Widget>[
+              _buildAppBar(),
+              SliverPadding(
+                padding: const EdgeInsets.all(16.0),
+                sliver: SliverList(
+                  delegate: SliverChildListDelegate(
+                    [
+                      // Total assets section
+                      SlideTransition(
+                        position: _offsetAnimation,
+                        child: _buildTotalAssetsSection(),
+                      ),
+                      const SizedBox(height: 32),
+                      // User breakdown section
+                      SlideTransition(
+                        position: _offsetAnimation,
+                        child: _buildUserBreakdownSection(),
+                      ),
+                      const SizedBox(height: 40),
+                      SlideTransition(
+                        position: _offsetAnimation,
+                        child: Row(
+                          children: [
+                            Text(
+                              'Connected Users',
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white,
+                                fontFamily: 'Titillium Web',
+                              ),
+                            ),
+                            Spacer(),
+                            Text(
+                              '(${client?.connectedUsers?.length})',
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white,
+                                fontFamily: 'Titillium Web',
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                      SlideTransition(
+                        position: _offsetAnimation,
+                        child:
+                            _buildConnectedUsersSection(),
+                      ),
+                      const SizedBox(height: 32),
+                      // Assets structure section
+                      SlideTransition(
+                        position: _offsetAnimation,
+                        child: _buildAssetsStructureSection(),
+                      ),
+                      const SizedBox(height: 132),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Positioned(
+            left: 0,
+            right: 0,
+            bottom: 0,
+            child: _buildBottomNavigationBar(context),
+          ),
+        ],
+      ),
+    );
+  
   }
+
+
+  SliverAppBar _buildAppBar() =>
+    SliverAppBar(
+      backgroundColor: const Color.fromARGB(255, 30, 41, 59),
+      automaticallyImplyLeading: false,
+      toolbarHeight: 80,
+      expandedHeight: 0,
+      snap: false,
+      floating: true,
+      pinned: true,
+      flexibleSpace: SafeArea(
+        child: Stack(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 20.0, right: 20.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Welcome Back, ${client?.firstName} ${client?.lastName}!',
+                    style: const TextStyle(
+                      fontSize: 23,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'Titillium Web',
+                    ),
+                  ),
+                  const SizedBox(height: 5),
+                  Text(
+                    'Client ID: ${client?.cid}',
+                    style: const TextStyle(
+                      fontSize: 15,
+                      color: Colors.white,
+                      fontFamily: 'Titillium Web',
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+      actions: [
+        Padding(
+          padding: const EdgeInsets.only(right: 5.0, bottom: 5.0),
+          child: GestureDetector(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const NotificationPage(),
+                ),
+              );
+            },
+            child: Container(
+              color: const Color.fromRGBO(239, 232, 232, 0),
+              padding: const EdgeInsets.all(10.0),
+              child: ClipRect(
+                child: Stack(
+                  children: <Widget>[
+                    Stack(
+                      alignment: Alignment.center,
+                      children: [
+                        Container(
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                              color: Colors
+                                  .transparent, // Change this color to the one you want
+                              width: 0.3, // Adjust width to your need
+                            ),
+                            shape: BoxShape
+                                .rectangle, // or BoxShape.rectangle if you want a rectangle
+                          ),
+                          child: Center(
+                            child: SvgPicture.asset(
+                              'assets/icons/bell.svg',
+                              colorFilter: const ColorFilter.mode(
+                                  Colors.white, BlendMode.srcIn),
+                              height: 32,
+                            ),
+                          ),
+                        ),
+                        Positioned(
+                          right: 0,
+                          top: 5,
+                          child: unreadNotificationsCount > 0
+                              ? Container(
+                                  decoration: BoxDecoration(
+                                    color: const Color(0xFF267DB5),
+                                    borderRadius: BorderRadius.circular(10),
+                                  ),
+                                  constraints: const BoxConstraints(
+                                    minWidth: 18,
+                                    minHeight: 18,
+                                  ),
+                                  child: Text(
+                                    '$unreadNotificationsCount',
+                                    style: const TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.w800,
+                                      fontFamily: 'Titillium Web',
+                                      fontSize: 12,
+                                    ),
+                                    textAlign: TextAlign.center,
+                                  ),
+                                )
+                              : Container(),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+
+  Widget _buildBottomNavigationBar(BuildContext context) => Container(
+        margin: const EdgeInsets.only(bottom: 30, right: 20, left: 20),
+        height: 80,
+        padding: const EdgeInsets.only(right: 10, left: 10),
+        decoration: BoxDecoration(
+          color: const Color.fromARGB(255, 30, 41, 59),
+          borderRadius: BorderRadius.circular(20),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.2),
+              spreadRadius: 8,
+              blurRadius: 8,
+              offset: const Offset(0, 3),
+            ),
+          ],
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            GestureDetector(
+              onTap: () {},
+              child: Container(
+                color: const Color.fromRGBO(239, 232, 232, 0),
+                padding: const EdgeInsets.all(20.0),
+                child: SvgPicture.asset(
+                  'assets/icons/dashboard_filled.svg',
+                  height: 22,
+                ),
+              ),
+            ),
+            GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  PageRouteBuilder(
+                    pageBuilder: (context, animation, secondaryAnimation) =>
+                        const AnalyticsPage(),
+                    transitionsBuilder:
+                        (context, animation, secondaryAnimation, child) =>
+                            child,
+                  ),
+                );
+              },
+              child: Container(
+                color: const Color.fromRGBO(239, 232, 232, 0),
+                padding: const EdgeInsets.all(20.0),
+                child: SvgPicture.asset(
+                  'assets/icons/analytics_hollowed.svg',
+                  height: 25,
+                ),
+              ),
+            ),
+            GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  PageRouteBuilder(
+                    pageBuilder: (context, animation, secondaryAnimation) =>
+                        const ActivityPage(),
+                    transitionsBuilder:
+                        (context, animation, secondaryAnimation, child) =>
+                            child,
+                  ),
+                );
+              },
+              child: Container(
+                color: const Color.fromRGBO(239, 232, 232, 0),
+                padding: const EdgeInsets.all(20.0),
+                child: SvgPicture.asset(
+                  'assets/icons/activity_hollowed.svg',
+                  height: 22,
+                ),
+              ),
+            ),
+            GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  PageRouteBuilder(
+                    pageBuilder: (context, animation, secondaryAnimation) =>
+                        const ProfilePage(),
+                    transitionsBuilder:
+                        (context, animation, secondaryAnimation, child) =>
+                            child,
+                  ),
+                );
+              },
+              child: Container(
+                color: const Color.fromRGBO(239, 232, 232, 0),
+                padding: const EdgeInsets.all(20.0),
+                child: SvgPicture.asset(
+                  'assets/icons/profile_hollowed.svg',
+                  height: 22,
+                ),
+              ),
+            ),
+          ],
+        ),
+      );
+
 }
