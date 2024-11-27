c03842149315e5be99da7bfbd56620229fb72f31
Omar Syed <omarsyed0504@gmail.com>
2024-11-27 17:34:24 -0500

    Load switch value during settings page initialization

--- lib/screens/profile/pages/settings.dart
+++ lib/screens/profile/pages/settings.dart
@@ -31,6 +31,7 @@ class _SettingsPageState extends State<SettingsPage> {
   @override
   void initState() {
     super.initState();
+    _loadSwitchValue();
     _checkNotificationPermission();
   }
 
